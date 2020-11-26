f50c42c92afa6b8346d502d69655d7d517cc4c4a
khanhhhh <nguyenngockhanh.pbc@gmail.com>
2020-11-26 23:02:28 +0800

    fixed

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 from board.board import Board
 from game import game
 
-shape = (30, 40)
-board = Board(
-    shape=shape,
-    obstacle=0.1,
-    customer=0.05,
-    salesman=0.01,
-)
-'''
-try:
-    board.obstacle.remove((0, 0))
-    board.customer.remove((0, 0))
-except ValueError as err:
-    print(err)
-board.salesman = [(0, 0)]
-'''
-game = game.Game(board, (20, 20))
+def main():
+    shape = (30, 40)
+    board = Board(
+        shape=shape,
+        obstacle=0.1,
+        customer=0.05,
+        salesman=0.01,
+    )
+    g = game.Game(board, (20, 20))
+    g.loop()
 
-game.loop()
+if __name__ == "__main__":
+    main()
