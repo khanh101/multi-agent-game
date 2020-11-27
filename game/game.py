a6b1e779c5c560367c6977760a60d581bd67e321
khanhhhh <nguyenngockhanh.pbc@gmail.com>
2020-11-28 03:53:59 +0800

    improved game

--- game/game.py
+++ game/game.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from typing import Tuple, List, Optional
 
 import pygame
@@ -25,6 +26,7 @@ def darken_and_blur(surface: pygame.Surface, amt: float = 30, opacity=200):
 
 class Game(object):
     board: Board
+    gui: bool
     cell_dim: Tuple[int, int]  # wh
     cell_size: Tuple[int, int]  # wh
     screen_size: Tuple[int, int]  # wh
@@ -37,90 +39,94 @@ class Game(object):
     youlose_surf: pygame.Surface = pygame.image.load("./game/assets/youlose_qr.png")
     screen: pygame.Surface
 
-    def __init__(self, board: Board, cell_size: Tuple[int, int] = (50, 50)):
-        height, width = board.shape
-        c_width, c_height = cell_size
-        screen_size = (c_width * width, c_height * height)
+    def __init__(self, board: Board, cell_size: Tuple[int, int] = None):
         self.board = board
-        self.cell_dim = board.shape[1], board.shape[0]
-        self.cell_size = cell_size
-        self.screen_size = screen_size
-        self.obstacle_surf = pygame.transform.scale(Game.obstacle_surf, cell_size)
-        self.customer_surf = pygame.transform.scale(Game.customer_surf, cell_size)
-        self.salesman_surf = pygame.transform.scale(Game.salesman_surf, cell_size)
-        self.salesman_surf_left = pygame.transform.scale(Game.salesman_surf_left, cell_size)
-        self.salesman_surf_right = pygame.transform.scale(Game.salesman_surf_right, cell_size)
-        self.youwin_surf = pygame.transform.scale(Game.youwin_surf, screen_size)
-        self.youlose_surf = pygame.transform.scale(Game.youlose_surf, screen_size)
-        self.screen = pygame.display.set_mode(size=screen_size)
+        self.gui = cell_size is not None
+        if self.gui:
+            height, width = board.shape
+            c_width, c_height = cell_size
+            screen_size = (c_width * width, c_height * height)
+            self.cell_dim = board.shape[1], board.shape[0]
+            self.cell_size = cell_size
+            self.screen_size = screen_size
+            self.obstacle_surf = pygame.transform.scale(Game.obstacle_surf, cell_size)
+            self.customer_surf = pygame.transform.scale(Game.customer_surf, cell_size)
+            self.salesman_surf = pygame.transform.scale(Game.salesman_surf, cell_size)
+            self.salesman_surf_left = pygame.transform.scale(Game.salesman_surf_left, cell_size)
+            self.salesman_surf_right = pygame.transform.scale(Game.salesman_surf_right, cell_size)
+            self.youwin_surf = pygame.transform.scale(Game.youwin_surf, screen_size)
+            self.youlose_surf = pygame.transform.scale(Game.youlose_surf, screen_size)
+            self.screen = pygame.display.set_mode(size=screen_size)
+
+    def get_salesman_surf(self, current: pygame.Surface, command: str) -> pygame.Surface:
+        text2salesmandsurf = {
+            "a": self.salesman_surf_left,
+            "w": self.salesman_surf,
+            "d": self.salesman_surf_right,
+            "s": self.salesman_surf,
+        }
+        return text2salesmandsurf.get(command, current)
 
     def loop(self, auto_controller: Optional[AutoController] = None) -> str:
-        output = ""
+        class State(Enum):
+            RUNNING = 0 # playing
+            QUIT = 1 # quit
+            ENDED = 2 # won or lost
+
+
         salesman_surf = self.salesman_surf
-        state = ""
-        running = True
-        step = 0
-        while running:
+        num_steps = 0
+        state: State = State.RUNNING
+        return_msg: str = ""
+
+        while state != State.QUIT:
+            # event handling
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    output = "q"
-                    running = False
+                    state = State.QUIT
                 if event.type == pygame.TEXTINPUT:
-                    if not (state == ""):
-                        if event.text in ["r", "q"]:
-                            output = event.text
-                            running = False
-                    else:
-                        if auto_controller is None:
-                            step += 1
-                            self.board.control_force(global_control(event.text))
-                            salesman_surf_dict = {
-                                "a": self.salesman_surf_left,
-                                "w": self.salesman_surf,
-                                "d": self.salesman_surf_right,
-                                "s": self.salesman_surf,
-                            }
-                            salesman_surf = salesman_surf_dict.get(event.text, salesman_surf)
-            if not (state == ""):
-                continue
-            obstacle_indices, customer_indices, salesman_indices = self.board.view()
-            win = len(customer_indices) == 0
-            lose = len(salesman_indices) == 0
-            state = ""
-            if lose:
-                state = "lose"
-            if win:
-                state = "win"
+                    if state == State.ENDED:
+                        if event.text in ("r", "q"):
+                            state = State.QUIT
+                            return_msg = event.text
+                    if state == State.RUNNING and auto_controller is None:
+                        num_steps += 1
+                        self.board.control_force(global_control(event.text))
+                        text2salesmandsurf = {
+                            "a": self.salesman_surf_left,
+                            "w": self.salesman_surf,
+                            "d": self.salesman_surf_right,
+                            "s": self.salesman_surf,
+                        }
+                        salesman_surf = text2salesmandsurf.get(event.text, salesman_surf)
+            #
+            if state == State.RUNNING:
+                # draw
+                obstacle_list, customer_list, salesman_list = self.board.view()
+                if len(customer_list) == 0:  # win
+                    self.screen.blit(darken_and_blur(self.screen), pygame.Rect((0, 0), self.screen_size))
+                    self.screen.blit(self.youwin_surf, pygame.Rect((0, 0), self.screen_size))
+                    state = State.ENDED
+                elif len(salesman_list) == 0:  # lose
+                    self.screen.blit(darken_and_blur(self.screen), pygame.Rect((0, 0), self.screen_size))
+                    self.screen.blit(self.youlose_surf, pygame.Rect((0, 0), self.screen_size))
+                    state = State.ENDED
+                else:  # running
+                    self.screen.fill((255, 255, 255))
+                    self.screen.blits(self.__blits_sequence(obstacle_list, self.obstacle_surf))
+                    self.screen.blits(self.__blits_sequence(customer_list, self.customer_surf))
+                    self.screen.blits(self.__blits_sequence(salesman_list, salesman_surf))
 
-            if state == "win":
-                self.screen.blit(darken_and_blur(self.screen), pygame.Rect((0, 0), self.screen_size))
-                self.screen.blit(self.youwin_surf, pygame.Rect((0, 0), self.screen_size))
-            if state == "lose":
-                self.screen.blit(darken_and_blur(self.screen), pygame.Rect((0, 0), self.screen_size))
-                self.screen.blit(self.youlose_surf, pygame.Rect((0, 0), self.screen_size))
+                pygame.display.flip()
 
-            if state == "":
-                self.screen.fill((255, 255, 255))
-                self.screen.blits(
-                    self.__blits_sequence(obstacle_indices, self.obstacle_surf),
-                    doreturn=False,
-                )
-                self.screen.blits(
-                    self.__blits_sequence(customer_indices, self.customer_surf),
-                    doreturn=False,
-                )
-                self.screen.blits(
-                    self.__blits_sequence(salesman_indices, salesman_surf),
-                    doreturn=False,
-                )
+                # control
+                if auto_controller is not None:
+                    num_steps += 1
+                    self.board.control_auto(auto_controller)
 
-            pygame.display.flip()
-            if auto_controller is not None and state == "":
-                step += 1
-                self.board.control_auto(auto_controller)
         pygame.quit()
-        print(f"finished in {step} steps")
-        return output
+        print(f"finished in {num_steps} steps")
+        return return_msg
 
     def __blits_sequence(self, indices: List[Tuple[int, int]], surface: pygame.Surface) -> List[
         Tuple[pygame.Surface, pygame.Rect]]:
